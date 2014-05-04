c83be3be5a4f82d56d2789a2ac336f720eaef8d0
Jan Xie <jan.h.xie@gmail.com>
2014-05-04 16:56:53 +0800

    benchmark pusher compliant server

--- README.markdown
+++ README.markdown
@@ -1,6 +1,22 @@
 # Peatio Slanger
 
-To run a local pusher server:
+## Start a local pusher server
 
     bundle
     bundle exec slanger --app_key foo --secret bar -p dev.key -c dev.crt  -v
+
+## Benchmark
+
+    # send 10 messages to 1000 concurrent clients, each message includes
+    # a 20 bytes payload.
+    ./benchmark.rb app_id app_key app_secret 1000 10 20
+
+## Tips
+
+Increase system limits (linux) with ulimit:
+
+    # check your hard limit on file descriptors
+    ulimit -Hn
+
+    # set your soft limit to max hard limit (suppose your hard limit is 4096)
+    ulimit -n 4096
--- benchmark.rb
+++ benchmark.rb
@@ -2,48 +2,92 @@
 
 require 'pusher'
 require 'pusher-client'
+require 'json'
+require 'uri'
 
-if ARGV.size != 6
-  puts "Usage: #{$0} app_id app_key app_secret number_of_clients number_of_messages payload_size"
+if ARGV.size != 8
+  puts "Usage: #{$0} api_server ws_server app_id app_key app_secret number_of_clients number_of_messages payload_size"
   exit 1
 end
 
-id     = ARGV[0]
-key    = ARGV[1]
-secret = ARGV[2]
-num    = ARGV[3].to_i
-total  = ARGV[4].to_i
-size   = ARGV[5].to_i
+api_server = URI(ARGV[0])
+ws_server  = URI(ARGV[1])
+id         = ARGV[2]
+key        = ARGV[3]
+secret     = ARGV[4]
+num        = ARGV[5].to_i
+total      = ARGV[6].to_i
+size       = ARGV[7].to_i
 
 PusherClient.logger = Logger.new File.open('pusher_client.log', 'w')
 
-channels = num.times.map do |i|
+stats = Hash.new {|h, k| h[k] = []}
+
+def puts_summary(stats, num, total, size, total_elapsed=nil)
+  latencies = stats.values.flatten
+  latency_avg = latencies.inject(&:+) / latencies.size
+  latency_mid = latencies.sort[latencies.size/2]
+
+  puts "\n*** Summary (clients: #{num}, messagen send: #{total}, payload size: #{size})***\n"
+  puts "Message received: %d (%.2f%%)" % [latencies.size, latencies.size.to_f*100/(num*total)]
+  puts "Total time: #{total_elapsed}s" if total_elapsed
+  puts "avg latency: #{latency_avg}s"
+  puts "min latency: #{latencies.min}s"
+  puts "max latency: #{latencies.max}s"
+  puts "mid latency: #{latency_mid}"
+end
+
+sockets = num.times.map do |i|
+  sleep 0.2
   received_total = 0
-  socket = PusherClient::Socket.new(key)
+  socket = PusherClient::Socket.new(
+    key,
+    ws_host: ws_server.host,
+    ws_port: ws_server.port,
+    wss_port: ws_server.port,
+    encrypted: ws_server.scheme == 'wss'
+  )
   socket.connect(true)
   socket.subscribe('benchmark')
   socket['benchmark'].bind('bm_event') do |data|
+    payload = JSON.parse data
+    latency = Time.now.to_f - payload['time'].to_f
+    stats[i] << latency
+
     received_total += 1
-    puts "#{i+1}.#{received_total}: #{data}"
+    puts "[#{i+1}.#{received_total}] #{data[0,60]}"
+
+    socket.disconnect if received_total == total
   end
-  socket['benchmark']
+
+  socket
 end
+
+channels = sockets.map {|s| s['benchmark'] }
 sleep 0.5 until channels.all?(&:subscribed)
+
+on_signal = ->(s) { puts_summary(stats, num, total, size); exit 0 }
+Signal.trap('INT',  &on_signal)
+Signal.trap('TERM', &on_signal)
 
 Pusher.app_id = id
 Pusher.key    = key
 Pusher.secret = secret
+Pusher.scheme = api_server.scheme
+Pusher.host   = api_server.host
+Pusher.port   = api_server.port
 
+ts = Time.now
 count = 0
 while count < total
   count += 1
-  payload = { time: Time.now, id: count, data: '*'*size }
+  payload = { time: Time.now.to_f.to_s, id: count, data: '*'*size }
   Pusher.trigger_async('benchmark', 'bm_event', payload)
-  sleep 1
+  sleep 0.5
 end
 
-Signal.trap('INT')  { exit 0 }
-Signal.trap('TERM') { exit 0 }
-loop do
-  sleep 1
-end
+threads = sockets.map {|s| s.instance_variable_get('@connection_thread') }
+threads.each(&:join)
+te = Time.now
+
+puts_summary(stats, num, total, size, te-ts)
